825c095e0c9c4e703b3ae0a9dac6317632744910
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-17 00:13:24 -0700

    PlacesList accepts a places prop

--- src/components/PlacesList/PlacesList.js
+++ src/components/PlacesList/PlacesList.js
@@ -2,29 +2,7 @@ import './PlacesList.css';
 
 import Place from './Place';
 
-const PlacesList = () => {
-  const places = [
-    {
-      id: 1,
-      name: "Dominos",
-      description: 'Foo bar baz bing, blah blah blah!'
-    },
-    {
-      id: 2,
-      name: "Pizza The Hutt",
-      description: 'Foo bar baz bing, blah blah blah!'
-    },
-    {
-      id: 3,
-      name: "Beto's",
-      description: 'Foo bar baz bing, blah blah blah!'
-    },
-    {
-      id: 4,
-      name: "Family Friendly Restaurant",
-      description: 'Foo bar baz bing, blah blah blah!'
-    }
-  ];
+const PlacesList = ({ places }) => {
 
   return (
     <div className='placesOuterContainer'>
