fc180dea9303af4971dddbc1274502dd9e4758b4
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-17 15:15:32 -0700

    Map WIP

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,16 +2,20 @@ import './Map.css'
 
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
-    const center = { lat: 40.2, lng: -111.65 }
+const Map = ({ coordinates }) => {
+  console.log({ coordinates });
+  const { lat, lng } = coordinates;
+  const center = { lat, lng }
 
-    const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY })
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY })
 
-    // Determine what to load
-    if (!isLoaded) return <div>Loading...</div>
-    return (
-      <GoogleMap className="map" zoom = { 12 } center={center} mapContainerClassName="map-container"></GoogleMap>
-    )
+  // Determine what to load
+  if (!isLoaded) return <div>Loading...</div>
+  return (
+    <GoogleMap className="map" zoom={12} center={center} mapContainerClassName="map-container">
+      <Marker position={{ lat: 40.2, lng: -111.62 }} />
+    </GoogleMap>
+  )
 };
 
 export default Map;
