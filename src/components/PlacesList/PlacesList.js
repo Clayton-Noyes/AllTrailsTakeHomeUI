971746e8b3dd6bde131ef4f56a27a415355a816a
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-17 15:15:58 -0700

    update PlacesList to be mobile friendly

--- src/components/PlacesList/PlacesList.js
+++ src/components/PlacesList/PlacesList.js
@@ -1,15 +1,30 @@
 import './PlacesList.css';
 
+import axios from 'axios';
+
 import Place from './Place';
 
-const PlacesList = ({ places }) => {
+const PlacesList = ({ places, coordinates }) => {
+  const { lat, lng } = coordinates
+  const body = { lat, lng, radius: 10000};
+
+  const googleDataClickHandler = async () => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/google_places`, { places_query: body })
+    console.log({ data })
+  }
 
   return (
     <div className='placesOuterContainer'>
+      <button 
+        className='getGoogleData'
+        onClick={googleDataClickHandler}
+      >
+        Grab Dat Data
+      </button>
       {places.length > 0 && places.map((obj) => (
         <Place
           key={obj.id}
-          restaurant={obj}
+          place={obj}
         />
       ))}
     </div>
