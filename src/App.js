76c871643456efc83a7a5135688edb4f45598daf
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-17 00:50:38 -0700

    get initial data in App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Header from './components/Header/Header';
 import PlacesList from './components/PlacesList/PlacesList';
 import Map from './components/Map/Map';
 
 const App = () => {
-  const places = [
-    {
-      id: 1,
-      name: "Dominos",
-      description: 'Foo bar baz bing, blah blah blah!'
-    },
-    {
-      id: 2,
-      name: "Pizza The Hutt",
-      description: 'Foo bar baz bing, blah blah blah!'
-    },
-    {
-      id: 3,
-      name: "Beto's",
-      description: 'Foo bar baz bing, blah blah blah!'
-    },
-    {
-      id: 4,
-      name: "Family Friendly Restaurant",
-      description: 'Foo bar baz bing, blah blah blah!'
-    }
-  ];
+  const [ initialData, setInitialData ] = useState('');
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  useEffect(() => {
+    // Set isLoading to true to indicate that the app is loading data
+    setIsLoading((prevState) => (!prevState));
+
+    // Declare the function that will get the initial data that will be used by the application
+    const getInitialData = async () => {
+      // Await axios to make the api call to the rails backend to get the data from the db
+      let { data } = await axios(`${process.env.REACT_APP_BACKEND_URL}/favorite_places.json`)
+      
+      // Set is loading to false and set the initialData
+      setIsLoading((prevState) => (!prevState));
+      setInitialData(data);
+    };
+    getInitialData()
+  }, [])
 
   return (
     <div className="layout">
       <Header />
-      <PlacesList places={places}/>
+      <PlacesList places={initialData}/>
       <Map />
     </div>
   )
