5e40f27590eab917fb0b79a0de2b46243c56a517
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-17 00:12:44 -0700

    include static data to App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ import PlacesList from './components/PlacesList/PlacesList';
 import Map from './components/Map/Map';
 
 const App = () => {
+  const places = [
+    {
+      id: 1,
+      name: "Dominos",
+      description: 'Foo bar baz bing, blah blah blah!'
+    },
+    {
+      id: 2,
+      name: "Pizza The Hutt",
+      description: 'Foo bar baz bing, blah blah blah!'
+    },
+    {
+      id: 3,
+      name: "Beto's",
+      description: 'Foo bar baz bing, blah blah blah!'
+    },
+    {
+      id: 4,
+      name: "Family Friendly Restaurant",
+      description: 'Foo bar baz bing, blah blah blah!'
+    }
+  ];
+
   return (
     <div className="layout">
       <Header />
-      <PlacesList />
+      <PlacesList places={places}/>
       <Map />
     </div>
   )
