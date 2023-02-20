fc0662501d37909b8cfdd0b4512f515f7f042dab
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-20 11:57:59 -0700

    include a searchquery change handler and use a searchQueryContext hook to provide values for the query

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,25 @@
+import { useContext } from 'react';
+
 // css for this component
 import './Header.css';
+import _debounce from 'lodash/debounce';
 
 // import the assets needed to display the All Trails logo
-import allTrailsWatermark from '../../assets/wordmark.png'
-import atLunchIcon from '../../assets/at-lunch.png'
-import magnifyingGlass from '../../assets/magnifying-glass.png'
+import allTrailsWatermark from '../../assets/wordmark.png';
+import atLunchIcon from '../../assets/at-lunch.png';
+import magnifyingGlass from '../../assets/magnifying-glass.png';
+
+import { SearchQueryContext } from '../Providers/SearchQueryContext';
 
 const Header = () => {
+  const [ searchQuery, setSearchQuery ] = useContext(SearchQueryContext);
+
+  const searchQueryChangeHandler = (event) => {
+    let newSearchQuery = event.target.value
+
+    setSearchQuery(newSearchQuery);
+  };
+
   return (
     <div className='header__Container'>
       <div className='header__InnerContainer'>
@@ -15,7 +28,11 @@ const Header = () => {
       </div>
       <div className='searchBox'>
         <img src={magnifyingGlass} />
-        <input className='searchBox__input' placeholder='Search restaurants' />
+        <input 
+          className='searchBox__input'
+          placeholder='Search restaurants'
+          onChange={_debounce(searchQueryChangeHandler, 300)}
+        />
       </div>
     </div>
   )
