9c438616c8d31c5704c3a41d81c495a3a3da8aee
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-20 11:59:36 -0700

    build a new SearchQuery Context and Provider component that can be used within the map and header components

---
+++ src/components/Providers/SearchQueryContext.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from 'react';
+
+const SearchQueryContext = createContext([{}, () => {}]);
+
+const SearchQueryProvider = (props) => {
+  const [state, setState] = useState({});
+
+  return (
+    <SearchQueryContext.Provider value={[ state, setState ]} >
+      { props.children }
+    </SearchQueryContext.Provider>
+  )
+}
+
+export { SearchQueryContext, SearchQueryProvider };
