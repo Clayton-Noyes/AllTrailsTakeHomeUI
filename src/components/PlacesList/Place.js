5312399660fe38d577dedfb901743fb3c646ccb7
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-20 11:59:04 -0700

    place component now knows if it is selected or not, and has the ability to set itself as the selected place or not

--- src/components/PlacesList/Place.js
+++ src/components/PlacesList/Place.js
@@ -5,17 +5,31 @@ import selectedBookmark from '../../assets/bookmark-saved.svg'
 
 import defaultLocationPic from '../../assets/default-place-image.png'
 
-const Place = ({ place }) => {
-  let { id, is_favorite, name, rating, description, number_of_reviews } = place;
+const Place = ({ 
+  place,
+  selectedIndex,
+  setSelectedPlace
+}) => {
+  let { object_id, is_favorite, name, rating, description, number_of_reviews } = place;
+  let isSelected = object_id === selectedIndex;
 
-  let placeContainerNames = "restaurantContainer" + (id === 1 ? " selected" : '');
+  let placeContainerNames = "restaurantContainer" + (isSelected ? " selected" : '');
   let reviews = `${number_of_reviews} Reviews`
+
+  const nameClickHandler = () => {
+    setSelectedPlace(object_id);
+  }
 
   return (
     <div className={ placeContainerNames }>
       <img className="restaurantImg" src={defaultLocationPic} />
       <div className="restaurantDetails__Container">
-        <span className="restaurantDetails__Name">{name}</span>
+        <button 
+          className="restaurantDetails__NameBtn"
+          onClick={nameClickHandler}
+        >
+          {name}
+        </button>
         <div className="restaurantDetails__ReviewInfo">
           <img className='restaurantDetails__GreenStar' src={greenStar} />
           <span className='restaurantDetails__Rating'>{rating}</span>
