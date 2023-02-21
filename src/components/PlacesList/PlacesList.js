a9592195888b10679a4062a32d0378cd116f7421
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-20 18:49:23 -0700

    remove the getGooglePlaces data functionality from the PlacesList component, as it is now in the header

--- src/components/PlacesList/PlacesList.js
+++ src/components/PlacesList/PlacesList.js
@@ -10,35 +10,17 @@ const PlacesList = ({
   places,
   isMobile,
   showList,
-  coordinates,
   showMapClickHandler,
   selectedPlace,
   setSelectedPlace,
-  updateAllPlaces,
   updatePlace
 }) => {
-  const { lat, lng } = coordinates
-  const body = { lat, lng, radius: 10000 };
-
-  const googleDataClickHandler = async () => {
-    const { data } = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/google_places`, { places_query: body })
-
-    updateAllPlaces(data);
-
-    console.log({data});
-  }
 
   return (
     <div className='placesOuterContainer'>
-      <button
-        className='getGoogleData'
-        onClick={googleDataClickHandler}
-      >
-        Grab Dat Data
-      </button>
       {places.length > 0 && places.map((obj) => (
         <Place
-          key={obj.id}
+          key={obj.object_id}
           place={obj}
           selectedPlace={selectedPlace}
           setSelectedPlace={setSelectedPlace}
