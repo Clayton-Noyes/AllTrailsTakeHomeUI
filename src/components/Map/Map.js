db57368fce021584a3cdef0d87a36f830b6d2ff7
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-02-20 18:48:15 -0700

    update the Map component to semi-final draft state

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,22 +1,41 @@
-import './Map.css'
+import './Map.css';
 
-import ListIcon from '../../assets/list-icon.svg'
+import ListIcon from '../../assets/list-icon.svg';
+import RestingMarkerIcon from '../../assets/pin-resting.png';
+import SelectedMarkerIcon from '../../assets/pin-selected.png';
 
-import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+// Need to use InfoWindowF components to display custom html and css when the marker is selected
+// import Place from '../../components/PlacesList/Place';
 
-const Map = ({ isMobile, places, coordinates, showMap, showListClickHandler }) => {
+import {
+  GoogleMap,
+  MarkerF,
+  useLoadScript,
+  // InfoWindowF
+} from '@react-google-maps/api';
+
+const Map = ({ 
+  isMobile,
+  coordinates,
+  places,
+  setCoordinates,
+  showListClickHandler,
+  selectedPlace,
+  setSelectedPlace,
+  showMap
+}) => {
   const { lat, lng } = coordinates;
-  const center = { lat, lng }
+  const center = { lat, lng };
 
-  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY })
+  // Start setting up the google map
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY });
 
-  // const mapClickHandler = (e) => {
-  //   console.log("map was clicked");
-  //   console.log(e);
+  // Provide a method for a Marker to change the selectedPlace when clicked
+  const markerClickHandler = (place) => {
+    setCoordinates({ lat: place.lat, lng: place.lng })
+    setSelectedPlace(place);
+  };
 
-  //   let x = 4;
-  // }
-  // Determine what to load
   if (!isLoaded) return <div>Loading...</div>
   return (
     <>
@@ -24,16 +43,24 @@ const Map = ({ isMobile, places, coordinates, showMap, showListClickHandler }) =
         mapContainerClassName="map-container map"
         zoom={15}
         center={center}
-        initialCenter={center}
-      // onClick={mapClickHandler}
       >
         {
-          places.map(place => (
-            <MarkerF
-              key={place.object_id}
-              position={{ lat: place.lat, lng: place.lng }}
-            />
-          ))
+          places.map(place => {
+            // TODO: For some reason when you select a new place, the icon is not updating... I am not sure why
+            //   this is. I have to research this and see if I can figure it out.
+            const isSelected = place.object_id === selectedPlace.object_id;
+            const markerImage = (isSelected) ? SelectedMarkerIcon : RestingMarkerIcon;
+
+            return (
+              <MarkerF
+                id={place.object_id}
+                key={place.object_id}
+                icon={markerImage}
+                onClick={() => (markerClickHandler(place))}
+                position={{ lat: place.lat, lng: place.lng }}
+              />
+            );
+          })
         }
       </GoogleMap>
       {
